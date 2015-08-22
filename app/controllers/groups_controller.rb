9a66c744bc6b3c0181afb4fa770664cd56d6248e
Ayaka Miyamoto <ayaka.miyamoto@rakus.co.jp>
2015-08-22 16:30:19 +0900

    modify select-tab

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,13 +3,15 @@ class GroupsController < ApplicationController
   # GET /groups
   # GET /groups.json
   def index
+    @groups_all = Group.all
     @groups = Group.all
-    @group = Group.where("name='"+params[:group_name]+"'") if params[:group_name].present?
+    @groups = Group.where("name='"+params[:group_name]+"'") if params[:group_name].present?
   end
 
   # POST /groups
   # POST /groups.json
   def create
+    @groups_all = Group.all
     @groups = Group.all
     @groups = @groups.where("name='"+params[:group_name]+"'") if params[:group_name].present?
   end
